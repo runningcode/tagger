93723f53bdbe985231e65c88c84245bdeb018e22
Nelson Osacky <nosacky@gradle.com>
2021-07-31 10:59:46 +0200

    Add publication preparation

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     `java-gradle-plugin`
     `maven-publish`
     id("com.gradle.plugin-publish") version "0.14.0"
+    signing
 }
 
 group = "com.osacky.tagger"
-version = "0.0.1"
+version = "0.1-SNAPSHOT"
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -17,19 +18,56 @@ gradlePlugin {
     plugins {
         create("tagger") {
             id = "com.osacky.tagger"
-            displayName = "Buildscan Tagging API"
-            description = "An easy way to tag Buildscans from Gradle Plugins"
             implementationClass = "com.osacky.scan.tag.ScanApiPlugin"
         }
     }
 }
 
+val isReleaseBuild : Boolean = !version.toString().endsWith("SNAPSHOT")
+
+val sonatypeUsername : String? by project
+val sonatypePassword : String? by project
+
 pluginBundle {
     tags = listOf("buildscan", "tags")
+
+    website = "https://github.com/runningcode/tagger"
+    vcsUrl = "https://github.com/runningcode/tagger"
+
+    description = "An easy way to tag build scans for Gradle plugin authors."
 
     mavenCoordinates {
         artifactId = "tagger-plugin"
         groupId = project.group.toString()
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (isReleaseBuild) releasesRepoUrl else snapshotsRepoUrl
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+    }
+
+    publications {
+        afterEvaluate {
+            named<MavenPublication>("pluginMaven") {
+                signing.sign(this)
+//                artifact(tasks["sourcesJar"])
+//                artifact(tasks["javadocJar"])
+                pom.configurePom("tagger")
+            }
+            named<MavenPublication>("taggerPluginMarkerMaven") {
+                signing.sign(this)
+                pom.configurePom("tagger")
+            }
+        }
     }
 }
 
@@ -49,12 +87,35 @@ tasks.withType<ValidatePlugins>().configureEach {
     enableStricterValidation.set(true)
 }
 
-tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.main.get().allSource)
-    archiveClassifier.set("sources")
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
-tasks.register<Jar>("javadocJar") {
-    from(tasks.javadoc)
-    archiveClassifier.set("javadoc")
+signing {
+    isRequired = isReleaseBuild
+}
+
+fun MavenPom.configurePom(pluginName: String) {
+    name.set(pluginName)
+    description.set("An easy way to tag build scans for Gradle plugin authors.")
+    url.set("https://github.com/runningcode/tagger")
+    licenses {
+        license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+        }
+    }
+    developers {
+        developer {
+            id.set("runningcode")
+            name.set("Nelson Osacky")
+        }
+    }
+    scm {
+        connection.set("scm:git:git://github.com/runningcode/tagger.git")
+        developerConnection.set("scm:git:ssh://github.com/runningcode/tagger.git")
+        url.set("https://github.com/runningcode/tagger")
+
+    }
 }
